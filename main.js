93765bc7840e6aada6be12a428a97a0cd08c252a
brunabendrot <108139327+brunascheiffer@users.noreply.github.com>
2024-08-02 17:55:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
- const numerosenha = document.querySelector('parametro-senha__texto');
+  const numerosenha = document.querySelector('parametro-senha__texto');
+let tamnhoSenha.textContent = tamanhoSenha12;
+numerosenha.textContent = tamnhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onlick = diminuitamanho;
+botoes[1].onlick = aumentatamanho;
+
+function diminuiTamanho(){
+    if(tamnhoSenha>1){
+        tamnhoSenha--;
+   
+}
+numerosenha.textContent = tamnhoSenha;
+}
+function aumentatamanho(){
+    if(tamanhoSenha<20){
+    tamanhoSenha++;
+    }
+    numerosenha.textContent = tamnhoSenha;
+} c
